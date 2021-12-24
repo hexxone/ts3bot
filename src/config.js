aa2144c71bca1d66733c1cd202fd320a4f33f69e
hexx.one <5312542+hexxone@users.noreply.github.com>
2021-12-25 00:19:07 +0100

    maintenance

--- src/config.js
+++ src/config.js
@@ -17,14 +17,7 @@ module.exports = self => {
     // Telegram only supports HTTPS connections to WebHooks.
     // Therefore, in order to use the bot with a WebHook, you will need a SSL certificate.
     // If you already setup Cloudflare with SSL for your domain, you usually dont need to do anything.
-    // Alternativce: Since August 29, 2015 Telegram supports self-signed certificates, here is how to generate one:
-    // 
-    // # Our private certificate will be the following, exported as "key.pem" - dont share this file!
-    // on linux: $ openssl genrsa -out key.pem 2048
-    // 
-    // # Our public certificate takes the "private" one as input and will be exported as "crt.pem"
-    // on linux: $ openssl req -new -sha256 -key key.pem -out crt.pem
-    //
+
     // Use Webhook? if set to false, polling is used
     self.useWebHook = process.env.WEBHOOK == "true" || false;
     // the address the webhook should use and where your server is reachable.
@@ -34,28 +27,37 @@ module.exports = self => {
     // Btw cloudflare will only support these aswell
     self.webHookPort = process.env.WEBHOOK_PORT || 443;
 
-    // Either generate a Cert in Cloudflare and save it locallly or
-    // generate a cert locally as above and set the file paths.
+	// Alternatively: Telegram supports self-signed certificates since 2015.
+	// After generating them, you should also set appropiate access permissions..
+    // 
+    // # Our private certificate will be the following, exported as "key.pem" - dont share this file!
+    // on linux: $ openssl genrsa -out key.pem 2048
+    // 
+    // # Our public certificate takes the "private" one as input and will be exported as "crt.pem"
+    // on linux: $ openssl req -new -sha256 -key key.pem -out crt.pem
+    //
     // private key
     self.webKey = __dirname + '/app/data/key.pem';
     // public key
     self.webCert = __dirname + '/app/data/crt.pem';
-    // When using self signed certificate (not Cloudflare), set to true
+    // When using self signed certificate (not external SSL), set to true
     self.webHookCustomCertificate = process.env.CUSTOM_CERT == "true" || false;
 
 
-    // Polling Config (only matters if not using webhook)
+    // Polling Config (only matters when not using webhook)
     self.pollTimeout = process.env.POLL_TIMEOUT || 65;
     self.pollInterval = process.env.POLL_INTERVAL || 1000;
 
 
     // File Proxy?
+	// will allow files shared in Telegram to be downloaded from your TeamSpeak users.
+	// Note: this will abviously expose the bot-ip to users.
     self.useFileProxy = process.env.FILE_PROXY == "true" || false;
     self.fileProxyPort = process.env.FILE_PROXY_PORT || 8080;
     self.fileProxyAddr = process.env.FILE_PROXY_ADDR || self.webHookAddr;
 
 
-    // The Telegram User ID of the Developer (for special commands)
+    // The Telegram User ID of the Developer (for admin & special commands)
     self.developer_id = process.env.DEVELOPER_ID || 12345678;
     // will send occuring Exceptions to the Developer if possible.
     self.debug = process.env.DEBUG == "true" || false;
@@ -65,5 +67,5 @@ module.exports = self => {
 
     // Bot per chat announcement
     self.announceID = process.env.MOTD_ID || 1;
-    self.announceText = process.env.MOTD_TXT || 'TS3Bot Announcement (this will only appear once):\r\nThe bot has changed a lot and is now back online :)\r\nIf you find a bug, please report it using /devmessage.\r\nThanks for reading!';
+    self.announceText = process.env.MOTD_TXT || 'TS3Bot Announcement (this will only appear once):\r\nPlease remember the TOS :)\r\nThanks for reading!';
 };
